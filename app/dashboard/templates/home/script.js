df3d00d1a50c09c219bc21e482ee2e21af57fa96
VietHTran <trantechenterprise@gmail.com>
2017-10-28 01:43:41 -0400

    Add Update

--- app/dashboard/templates/home/script.js
+++ app/dashboard/templates/home/script.js
@@ -4,20 +4,118 @@
 // Polyfills to finish for more complex Web Components in Polymer.
 window.addEventListener('WebComponentsReady', function() {
     $('#wait-for-it').show();
+    $("#refresh-button").css("visibility", "visible");
+    $("#get-link-button").css("visibility", "visible");
 
-    // Select sensors and nodes for initial load
-    $.get({
-        url: '/api/1.0/datapoints',
-        data: {
-            "sensor": "HUMA,TCA",
-            "node": "/node/762b8ff0-8679-11e6-a353-2f6c041e2491"
-        },
-    }).done(function(data) {
-        $('#wait-for-it').hide();
-        $('#volcanograph')[0].chartData = data;
-    }).fail(function(err) {
-        alert("Error: Unable to fetch initial node and sensor data.  Try again.");
-    });
+    /*
+     *Get HTML representation of units saved in database (ex: celcius-->C)
+     *
+     *unit: unit string name get from database
+     *return: HTML string representation of units
+     * */
+    var getFormattedUnit = function(unit) {
+        switch (unit) {
+            case "celcius":
+                return "C";
+            case "relative humidity":
+                return "RH";
+            case "percentage":
+                return "%";
+            case "pascal":
+                return "Pa";
+        }
+        return unit;
+    };
+
+    /*
+     *Get datapoints for initial data
+     * */
+    var getInitialData = function() {
+        var nodeId = $('#node_id_span').html();
+        var nodeUri = '/node/'+nodeId;
+        var sensorTagsSpan = $('#sensor_tags_span').html();
+        // Select sensors and nodes for initial load
+        $.get({
+            url: '/api/1.0/datapoints',
+            data: {
+                "sensor": sensorTagsSpan,
+                "node": nodeUri
+            },
+        }).done(function(data) {
+            $('#volcanograph')[0].chartData = data;
+            $('#wait-for-it').hide();
+            $('#sensor_tags_span').html(sensorTagsSpan);
+            $('#node_id_span').html(nodeId);
+        }).fail(function(err) {
+            $('#wait-for-it').hide();
+            alert("Error: Unable to fetch initial node and sensor data. Try again.");
+        });
+    }
+
+    /*
+     *Get new datapoints 
+     * 
+     *data: an object includes and array of selected sensor tags and node id
+     * */
+    var getRefreshData = function(data) {
+        // Select sensors and nodes for initial load
+        var sensorTags = data.sensors.join(",");
+        var nodeUri = data.node;
+        $.get({
+            url: '/api/1.0/datapoints',
+            data: {
+                "sensor": sensorTags,
+                "node": nodeUri 
+            },
+        }).done(function(data) {
+            $('#volcanograph')[0].chartData = data;
+            $('#wait-for-it').hide();
+            $('#sensor_tags_span').html(sensorTags);
+            var tmp = nodeUri.split("/");
+            for (var i = tmp.length-1; i>=0; i--) {
+                if (tmp[i] && tmp[i] !== "" && tmp[i] !== "node") {
+                    $('#node_id_span').html(tmp[i].toString());
+                }
+            }
+        }).fail(function(err) {
+            $('#wait-for-it').hide();
+            alert("Unable to fetch data for this node and sensors.");
+        });
+    }
+    
+    /*
+     *Get datatypes from selected sensor
+     *
+     *sensors: array of selected sensor tags
+     *onSuccess: a function called after successfully fetch datatypes 
+     *onSuccessParam: parameter for onSuccess function if applied
+     * */
+    var getDatatypes = function(sensors, onSuccess, onSuccessParam) {
+        $.get({
+            url: '/api/1.0/datatypes',
+        }).done(function(data) {
+            var allTagUnit = {}; // Store all datatype tags as keys and units as values
+            $('#volcanograph')[0].seriesConfig = {};
+            seriesConfig = $('#volcanograph')[0].seriesConfig;
+            for (var i in data) {
+                allTagUnit[data[i].tag] = {yAxisUnit: data[i].unit};
+            }
+            for (var i in sensors) {
+                if (sensors[i] in allTagUnit) {
+                    var tag = sensors[i];
+                    allTagUnit[tag].yAxisUnit = getFormattedUnit(allTagUnit[tag].yAxisUnit);
+                    seriesConfig[tag] = allTagUnit[tag];
+                }
+            }
+            onSuccess(onSuccessParam);
+        }).fail(function(err) {
+            $('#wait-for-it').hide();
+            alert("Error: Unable to fetch datatypes");
+        });
+    };
+
+    // Load initial data
+    getDatatypes($('#sensor_tags_span').html().split(","), getInitialData);
 
     $("#refresh-button").click(function(evt) {
         // Show spinner that we're working
@@ -33,27 +131,45 @@ window.addEventListener('WebComponentsReady', function() {
         }
 
         // Check which nodes are selected
-        var node = $('#node')[0].selectedKey
-
-        if (node.length == 0 || sensors.length == 0) {
+        var selectedNode = $('#node')[0].selectedKey;
+        if (selectedNode.length == 0 || sensors.length == 0) {
             alert("Select at least one node and sensor to display.");
+            $('#wait-for-it').hide();
             return false;
         }
-
-        // Refresh chart data with new datapoints
-        $.get({
-            url: '/api/1.0/datapoints',
-            data: {
-                "sensor": sensors.join(),
-                "node": node,
-            },
-        }).done(function(data) {
-            $('#volcanograph')[0].chartData = data;
-            $('#wait-for-it').hide();
-        }).fail(function(err) {
-            alert("Unable to fetch data for this node and sensors.");
-        });
+        var data = {};
+        data.sensors = sensors;
+        data.node = selectedNode;
+        getDatatypes(sensors, getRefreshData, data);
     });
 
+    $("#get-link-button").click(function(evt) {
+        var nodeId = $('#node_id_span').html();
+        var sensorTagsSpan = $('#sensor_tags_span').html();
+        var hostname = window.location.hostname;
+        if (hostname==="localhost" || hostname==="127.0.0.1") { // when debugging
+            hostname += ":" + window.location.port;
+        }
+        var url = hostname + "/node/" + nodeId + "/" + sensorTagsSpan;
+
+        // Copy url to clipboard
+        var container = document.createElement('div');
+        container.innerHTML = url;
+
+        // Hide element
+        container.style.position = 'fixed';
+        container.style.pointerEvents = 'none';
+        container.style.opacity = 0;
+
+        document.body.appendChild(container);
+
+        window.getSelection().removeAllRanges();
+
+        var range = document.createRange();
+        range.selectNode(container);
+        window.getSelection().addRange(range);
+        document.execCommand('copy');
+        document.body.removeChild(container);
+    });
 });
 </script>
--- app/dashboard/views.py
+++ app/dashboard/views.py
@@ -1,10 +1,9 @@
-
 import re
 import json
-from flask import render_template
-
+from flask import render_template, request, make_response
+import httplib2
+import json
 import predix.data.asset
-
 from . import dashboard
 
 asset = predix.data.asset.Asset()
@@ -17,6 +16,14 @@ def natural_sort(l):
     alphanum_key = lambda key: [ convert(c) for c in re.split('([\d]+)', key['val']) ]
     return sorted(l, key=alphanum_key)
 
+# Genereate simple response for GET request
+# message: displayed string
+# code: status code
+def generateResponse(message, code):
+    response = make_response(json.dumps(message), code)
+    response.headers['Content-Type'] = 'application/json'
+    return response
+
 @dashboard.route('/')
 def home():
     """
@@ -26,6 +33,9 @@ def home():
     # Query the node assets we tracked
     nodes = []
     default_node = ''
+    default_node_name = ''
+    node_id = ''
+    sensor_tags = 'HUMA,TCA' # Default sensors
     for node in asset.get_collection('/node'):
         nodes.append({
             "key": node['uri'],
@@ -33,6 +43,8 @@ def home():
             })
         if node['name'] == 'N10':
             default_node = node['uri']
+            default_node_name = node['name']
+            node_id = default_node.split("/")[-1] # get node id from uri
 
     # Query the sensor assets we tracked
     sensors = []
@@ -46,4 +58,57 @@ def home():
     return render_template('home.html',
             sensors=json.dumps(natural_sort(sensors)),
             default_node=default_node,
-            nodes=json.dumps(natural_sort(nodes)))
+            default_node_name=default_node_name,
+            node_id=node_id,
+            sensor_tags=sensor_tags,
+            nodes=json.dumps(natural_sort(nodes))) 
+
+@dashboard.route('/node/<node_id>/<sensor_tags>/', methods=['GET'])
+def node(node_id, sensor_tags):
+    """
+    For this route we display a simple volcano dashboard to view specified node and
+    sensor data in a time series graph.
+    """
+    # Query the node assets we tracked
+    nodes = []
+    default_node = ''
+    default_node_name = ''
+    is_valid_node_id = False # used to validate node_id
+    sensor_tags_lst = sensor_tags.split(",") # store sensor ids  as list
+    node_uri = '/node/' + node_id
+    for node in asset.get_collection('/node'):
+        nodes.append({
+            "key": node['uri'],
+            "val": node['name']
+            })
+        if node['name'] == 'N10':
+            default_node = node['uri']
+        if node_uri == node['uri']: # if node uri matched then node id is valid
+            is_valid_node_id = True
+            default_node = node_uri
+            default_node_name = node['name']
+
+    if not is_valid_node_id:
+        return generateResponse("Invalid node id", 401)
+
+    # Query the sensor assets we tracked
+    sensors = []
+    for sensor in asset.get_collection('/datatype'):
+        sensors.append({
+            "key": sensor['tag'],
+            "val": sensor['data_type'] + " (%s)" % (sensor['tag'])
+            })
+        if sensor['tag'] in sensor_tags_lst: # Remove valid tag
+            sensor_tags_lst.remove(sensor['tag'])
+
+    if sensor_tags_lst != []: # sensor tag lists is not empty --> has invalid tags
+        return generateResponse("Invalid sensor tag", 401)
+
+    # Render the dashboard jinja2 template
+    return render_template('home.html',
+            sensors=json.dumps(natural_sort(sensors)),
+            default_node=default_node,
+            default_node_name=default_node_name,
+            node_id=node_id,
+            sensor_tags=sensor_tags,
+            nodes=json.dumps(natural_sort(nodes))) 
